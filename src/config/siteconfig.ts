87c0ef95a3c4da37479168f15bf8e836faee8511
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-08-16 09:42:02 +0530

    chore added site config file

---
+++ src/config/siteconfig.ts
@@ -0,0 +1,11 @@
+export default {
+  name: "Developer blogs",
+  url: "theabhipatel.com",
+  description: "this is the developers bloging website created by theabhipatel",
+  author: "The Abhi Patel",
+  links: {
+    github: "https://github.com/theabhipatel",
+    linkedIn: "https://www.linkedin.com/in/theabhipatel",
+    personalSite: "https://theabhipatel.com",
+  },
+};
