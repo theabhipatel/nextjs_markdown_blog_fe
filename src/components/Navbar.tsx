46aec959fc100709ab49e51428d265c90e3e5c3e
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-08-16 09:43:14 +0530

    feat created a navbar component and added it in layout and working on navbar...

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">Navbar</div>
+    </header>
+  );
+};
+
+export default Navbar;
